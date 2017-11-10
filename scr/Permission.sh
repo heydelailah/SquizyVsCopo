95ca6e02f699ff5d836b28634433d7bd063471ff
AgenciaEstacioFib <AgenciaEstacioFib@gamil.com>
2017-11-10 12:25:02 -0500

    Fri Nov 10 12:25:02 EST 2017 by Agencia

---
+++ scr/Permission.sh
@@ -0,0 +1,6 @@
+#Ubuntu:
+#using if and ele then solve it
+sudo chown cabox -R /home/cabox/workspace 
+
+#CentOS:
+#sudo chown cabox:apache -R ~/workspace
---
+++ scr/agenc.sh
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "`date` by Agencia"
+git push
